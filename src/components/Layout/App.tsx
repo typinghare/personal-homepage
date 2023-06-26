6774c4f72d3bc6974bad3ecbb1c4ad9c0063de0e
James Chan <jameschan312.cn@gmail.com>
2023-06-26 00:28:47 -0400

    initial

--- src/components/Layout/App.tsx
+++ src/components/Layout/App.tsx
@@ -1,9 +1,36 @@
 import { JSX } from 'react'
+import { Alert, AlertTitle, Container, Link } from '@mui/material'
+import { collectStyles } from '../../common/functions/style'
 
 export function App(): JSX.Element {
+    const styles = collectStyles({
+        root: {
+            paddingTop: '2em',
+            minHeight: '100vh',
+        },
+    })
+
     return (
-        <>
-            Hello World.
-        </>
+        <Container sx={styles.root}>
+            <Alert severity='info'>
+                <AlertTitle>
+                    Statement
+                </AlertTitle>
+
+                Hi, my name is James, a junior full stack developer, and this is my personal website.
+                I like writing web application in my leisure time, and deploying them on my personal website.
+                The homepage of this website is still under construction, but you can check out my applications
+                by following links: <br />
+
+                <ul>
+                    <li>
+                        <Link href='https://jameschan.us/bgc'>Board Game Clock</Link>
+                    </li>
+                    <li>
+                        <Link href='https://jameschan.us/supervisor/space/1'>Supervisor 2 (My Space)</Link>
+                    </li>
+                </ul>
+            </Alert>
+        </Container>
     )
 }
